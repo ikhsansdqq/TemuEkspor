15224b18e57497b96031bde002635d52def8e920
Ikhsan Assidiqie <ikhsansdqq@gmail.com>
2024-06-16 05:36:06 +0700

    #1 Issue Conflict Home

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,10 @@
 import Link from "next/link";
 
-import Hero from "../components/hero";
-import SearchBox from "../components/searchbox";
-
 export default function Home() {
   return (
     <div>
-      <SearchBox />
-      <Hero/>
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">Temu Ekspor</h1>
+      <p className="text-base text-gray-900 mb-4">One Stop Solution for Your Needs</p>
       <Link href="/profile">
         <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Go to Profile</button>
       </Link>
